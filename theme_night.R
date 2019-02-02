35973082e608ff0f876657d0ad2e36c6a5bef299
Andreas Domberg <andom@posteo.de>
2019-02-02 13:48:57 +0100

    Revert "set default color and fill to white"
    This reverts commit 11a2f65001ce8ab2f5d0a8f24447b349a4c8f334.
    detach-reattach procedure does not work if another R package requires ggplot2.
    rstan does.  So prefer to set white color/fill manually.

--- theme_night.R
+++ theme_night.R
@@ -1,42 +1,7 @@
 theme_nightout <- function(base_size = 11,
-                          base_family = "", ...) {
-  
-  # if using only one colour, set to white, given black bgr:
-  update_geom_defaults("curve", list(colour = "white"))
-  update_geom_defaults("path", list(colour = "white"))
-  update_geom_defaults("polygon", list(colour = "white", fill = "white"))
-  update_geom_defaults("rect", list(colour = "white", fill = "white"))
-  update_geom_defaults("ribbon", list(colour = "white", fill = "white"))
-  update_geom_defaults("abline", list(colour = "white"))
-  update_geom_defaults("hline", list(colour = "white"))
-  update_geom_defaults("vline", list(colour = "white"))
-  update_geom_defaults("segment", list(colour = "white"))
-  update_geom_defaults("spoke", list(colour = "white"))
-  update_geom_defaults("area", list(colour = "white", fill = "white"))
-  update_geom_defaults("density", list(colour = "white", fill = "white"))
-  update_geom_defaults("dotplot", list(colour = "white", fill = "white"))
-  update_geom_defaults("bar", list(colour = "white"))
-  update_geom_defaults("label", list(colour = "white"))
-  update_geom_defaults("point", list(colour = "white", fill = "white"))
-  update_geom_defaults("quantile", list(colour = "white"))
-  update_geom_defaults("rug", list(colour = "white"))
-  update_geom_defaults("smooth", list(colour = "white", fill = "white"))
-  update_geom_defaults("text", list(colour = "white"))
-  update_geom_defaults("col", list(colour = "white", fill = "white"))
-  update_geom_defaults("boxplot", list(colour = "white", fill = "white"))
-  update_geom_defaults("violin", list(colour = "white", fill = "white"))
-  update_geom_defaults("hex", list(colour = "white", fill = "white"))
-  update_geom_defaults("line", list(colour = "white"))
-  update_geom_defaults("step", list(colour = "white"))
-  update_geom_defaults("crossbar", list(colour = "white", fill = "white"))
-  update_geom_defaults("errorbar", list(colour = "white"))
-  update_geom_defaults("errorbarh", list(colour = "white"))
-  update_geom_defaults("linerange", list(colour = "white"))
-  update_geom_defaults("pointrange", list(colour = "white", fill = "white"))
-  update_geom_defaults("map", list(colour = "white", fill = "white"))
-  
-  p <- theme_grey(base_size = base_size,
-                  base_family = base_family) %+replace%
+                          base_family = "") {
+  theme_grey(base_size = base_size,
+             base_family = base_family) %+replace%
     theme(
       # get rid of panel grids
       panel.grid.major = element_blank(),
@@ -67,56 +32,15 @@ theme_nightout <- function(base_size = 11,
       strip.background = element_rect(fill = NA,
                                       color = NA),
       strip.text = element_text(color = "white")
-    ) +
-    theme(...)
-  
-  # detach & reattach ggplot2:
-  detach("package:ggplot2", unload = TRUE)
-  library(ggplot2)
-  
-  return(p)
+      
+    )
 }
 
 theme_nightin <- function(base_size = 11,
                           base_family = "",
-                          legend.position = c(.9, .8), ...) {
-  
-  # if using only one colour, set to white, given black bgr:
-  update_geom_defaults("curve", list(colour = "white"))
-  update_geom_defaults("path", list(colour = "white"))
-  update_geom_defaults("polygon", list(colour = "white", fill = "white"))
-  update_geom_defaults("rect", list(colour = "white", fill = "white"))
-  update_geom_defaults("ribbon", list(colour = "white", fill = "white"))
-  update_geom_defaults("abline", list(colour = "white"))
-  update_geom_defaults("hline", list(colour = "white"))
-  update_geom_defaults("vline", list(colour = "white"))
-  update_geom_defaults("segment", list(colour = "white"))
-  update_geom_defaults("spoke", list(colour = "white"))
-  update_geom_defaults("area", list(colour = "white", fill = "white"))
-  update_geom_defaults("density", list(colour = "white", fill = "white"))
-  update_geom_defaults("dotplot", list(colour = "white", fill = "white"))
-  update_geom_defaults("bar", list(colour = "white"))
-  update_geom_defaults("label", list(colour = "white"))
-  update_geom_defaults("point", list(colour = "white", fill = "white"))
-  update_geom_defaults("quantile", list(colour = "white"))
-  update_geom_defaults("rug", list(colour = "white"))
-  update_geom_defaults("smooth", list(colour = "white", fill = "white"))
-  update_geom_defaults("text", list(colour = "white"))
-  update_geom_defaults("col", list(colour = "white", fill = "white"))
-  update_geom_defaults("boxplot", list(colour = "white", fill = "white"))
-  update_geom_defaults("violin", list(colour = "white", fill = "white"))
-  update_geom_defaults("hex", list(colour = "white", fill = "white"))
-  update_geom_defaults("line", list(colour = "white"))
-  update_geom_defaults("step", list(colour = "white"))
-  update_geom_defaults("crossbar", list(colour = "white", fill = "white"))
-  update_geom_defaults("errorbar", list(colour = "white"))
-  update_geom_defaults("errorbarh", list(colour = "white"))
-  update_geom_defaults("linerange", list(colour = "white"))
-  update_geom_defaults("pointrange", list(colour = "white", fill = "white"))
-  update_geom_defaults("map", list(colour = "white", fill = "white"))
- 
-  p <- theme_grey(base_size = base_size,
-                  base_family = base_family) %+replace%
+                          legend.position = c(.9, .8)) {
+  theme_grey(base_size = base_size,
+             base_family = base_family) %+replace%
     theme(
       # get rid of panel grids
       panel.grid.major = element_blank(),
@@ -147,12 +71,6 @@ theme_nightin <- function(base_size = 11,
       strip.background = element_rect(fill = NA,
                                       color = NA),
       strip.text = element_text(color = "white")
-    ) +
-    theme(...)
-  
-  # detach & reattach ggplot2:
-  detach("package:ggplot2", unload = TRUE)
-  library(ggplot2)
-  
-  return(p)
+      
+    )
 }
